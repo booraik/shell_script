d58a46c843bef55f0a7efa0cf2c9809543935075
booraik <booraik@hotmail.com>
2015-08-13 13:34:23 +0900

    Add Backup Script.

---
+++ backup.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+BACKUP_DIR=/var/backup/
+BACKUPLIST_FILE=/var/backup/backup_files.txt
+DATE=`date "+%y%m%d"`
+BACKUP_DIR_TODAY=$BACKUP_DIR$DATE
+
+#make directory
+if [ ! -e $BACKUP_DIR_TODAY ]; then
+    mkdir $BACKUP_DIR_TODAY
+fi
+
+while read line
+do
+    if [ ${#line} -eq 0 ]; then
+        continue;
+    fi
+
+    if [ ${line:0:1} = '#' ]; then
+        continue;
+    fi
+
+    /bin/cp -f $line $BACKUP_DIR_TODAY/
+
+done < $BACKUPLIST_FILE
