9569411e2def435224bae9af55c69a8141be1d00
booraik <booraik@hotmail.com>
2017-11-01 13:19:04 +0900

    Update and rename change_music_tag to automation/change_mp3_tag_encoding.sh
    mp3 파일들의 태그값 변경. CP949 --> UTF

--- automation/change_mp3_tag_encoding.sh
+++ automation/change_mp3_tag_encoding.sh
@@ -1,7 +1,6 @@
-
+#!/bin/bash
+# Change mp3 encoding cp949 to UTF
 # Reference : http://egloos.zum.com/mcchae/v/11174534
 # needed mutagen python package : pip install mutagen
-
-DIR=$1
 
 find $1 -iname "*.mp3" -execdir mid3iconv -e cp949 {} \;
