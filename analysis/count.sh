665d73dff879c163944d6b86f91b8c46a6bdc176
booraik <booraik@hotmail.com>
2017-11-01 11:40:35 +0900

    Update count.sh
    httpd 로그파일을 파싱하여 요청이 적은 순으로 IP 주소와 요청 횟수를 출력해주는 스크립트

--- analysis/count.sh
+++ analysis/count.sh
@@ -1,6 +1,13 @@
-#!/bin/bash
-# count log 
-#Usage: ./count.sh <file> <index>
-FILE=${1:-access_log*}
-INDEX=${2:-1}
+
+# count ip address who accessed from httpd log files
+
+# Usage: ./count.sh <file> <index>
+#       <file> : support wildcard
+#       <index> : IP address index in log file at seperate by space( " " )
+
+# Example : ./count "/var/log/httpd/ssl_access_log*"
+#           ./count "/var/log/httpd/ssl_request_log*" 3
+
+FILE=${1:-access_log*}  # Default : access-log*
+INDEX=${2:-1}           # Default : 1
 cat $FILE | awk "{print $ $INDEX}" | sort |uniq -dc | sort -n
