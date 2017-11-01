36c2068e1c360f28cea6e7fe0e66116dc804dd2a
booraik <booraik@hotmail.com>
2017-11-01 13:22:10 +0900

    Update and rename backup.sh to automation/backup.sh

--- automation/backup.sh
+++ automation/backup.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# Backup file to BACKUP_DIR what BACKUPLIST_FILE file
+# Change backup directory everyday based date
 
 BACKUP_DIR=/var/backup/
 BACKUPLIST_FILE=/var/backup/backup_files.txt
